e0f435ff15979f1fb0c48bcc0c7eee7b2b63b020
Todd Kennedy <todd@selfassembled.org>
2021-03-19 19:09:10 -0700

    array.from please

--- assets/links.js
+++ assets/links.js
@@ -1,4 +1,4 @@
-for (const post of Array.toArray(document.querySelectorAll('.post'))) {
+for (const post of Array.from(document.querySelectorAll('.post'))) {
   const url = post.dataset.canonical
   const title = post.querySelector('h2')
   const a = document.createElement('a')
