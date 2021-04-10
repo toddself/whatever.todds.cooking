efea5f5311ca4740cdffef86777c3f1e5838fc77
Todd Kennedy <todd@selfassembled.org>
2021-04-10 11:11:44 -0700

    handle image conversion

--- build.sh
+++ build.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
+
+# make blog
 DENO_CMD="${HOME}/.deno/bin/deno"
 [ ! -f $DENO_CMD ] && curl -fsSL https://deno.land/x/install/install.sh | sh
 $DENO_CMD run --allow-read --allow-write ./src/builder.ts --src data --dest dist
+
+# handle images
+shopt -s nullglob dotglob
+if [ -e $PWD/raw_images ]; then
+  RAW_FILE_LIST=($PWD/raw_images/*)
+  if [ ${#RAW_FILE_LIST[@]} -gt 0 ]; then
+    echo $RAW_FILE_LIST
+    for IMG in $RAW_FILE_LIST; do
+      BASE=$(basename $IMG)
+      DEST="assets/${BASE}"
+      convert "${IMG}" -resize 1600x1600 -quality 40 -strip "${DEST}"
+    done
+  fi
+fi
+
+# copy assets
 cp -R assets/* dist/.
